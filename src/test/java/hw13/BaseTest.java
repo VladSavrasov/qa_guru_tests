1ffbf2c887a7d858622b5ade6ef05769634ae5de
Vlad Savrasov <vsvsvs08@rambler.ru>
2023-10-30 16:19:25 +0300

    hw13

--- src/test/java/hw13/BaseTest.java
+++ src/test/java/hw13/BaseTest.java
@@ -2,16 +2,26 @@ package hw13;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
     @BeforeEach
-    public void baseTest(){
+    public void baseTest() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
-        SelenideLogger.addListener("allure",new AllureSelenide());
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
