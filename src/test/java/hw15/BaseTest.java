e974db74f85ad6b21bb43042d5e9d015b4a4ae5e
Vlad Savrasov <vsvsvs08@rambler.ru>
2023-11-12 18:18:47 +0300

    hw 15 add capabilities

--- src/test/java/hw15/BaseTest.java
+++ src/test/java/hw15/BaseTest.java
@@ -16,24 +16,32 @@ public class BaseTest {
 
     @BeforeEach
     public void baseTest() {
-        Configuration.headless = true;
+//        Configuration.headless = true;
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "99.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
-        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.remote = "https://user1:1234@" + remoteUrl + "/wd/hub";
 
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//
+//        Configuration.browserCapabilities = capabilities;
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", System.getProperty("browser"));
+        capabilities.setCapability("browserVersion",System.getProperty("browserVersion"));
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
-
         Configuration.browserCapabilities = capabilities;
-
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
+
 
     @AfterEach
     void addAttachments() {
