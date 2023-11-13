b12f698237b3c1a68f3820e3fae69f2a26839a9b
Vlad Savrasov <vsvsvs08@rambler.ru>
2023-11-13 21:28:07 +0300

    hw 15 add capabilities

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -47,7 +47,8 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = "https://"+ remoteUrl + sessionId() + ".mp4";
+        String remoteWebDriver = System.getProperty("remoteWebDriver", "selenoid.autotests.cloud");
+        String videoUrl = "https://" + remoteWebDriver + "/video/" + sessionId() + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
